036594a379db440bfa1a03ac8d58df5d82ef050c
Akshay <rathodak315@gmail.com>
2022-09-14 16:46:35 +0530

    testing

--- src/Pages/HomePage_Folder/HomePage.js
+++ src/Pages/HomePage_Folder/HomePage.js
@@ -16,24 +16,24 @@ const HomePage = () => {
     return (
         <section className="bg-SecondColor">
             <div className="container pl-3 md:pl-20 lg:pl-32 min-h-screen ">
-               <div className="font-Poppins pt-28 md:pt-52 ">
-               <h4 >HI, my name is</h4>
-                <h1 className="md:text-5xl text-3xl font-bold my-6  flex-1 " >Akshay Rathod.</h1>
-                <h1 className="md:text-6xl text-4xl font-bold mb-5"  >I build things for the web</h1>
-                <p className="w-full md:w-1/2" >I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences.Currently, I'm focused on building accessible, human-centered products at Upstatement.</p>
+                <div className="font-Poppins pt-28 md:pt-52 ">
+                    <h4 >HI, my name is</h4>
+                    <h1 className="md:text-5xl text-3xl font-bold my-6  flex-1 " >Akshay Rathod.</h1>
+                    <h1 className="md:text-6xl text-4xl font-bold mb-5"  > I build things for the web test</h1>
+                    <p className="w-full md:w-1/2" >I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences.Currently, I'm focused on building accessible, human-centered products at Upstatement.</p>
 
-                <div className="pt-10">
-                <button className="btn" ><a href=" mailto:akshaycoder07@gmail.com">Get In Touch</a></button>
+                    <div className="pt-10">
+                        <button className="btn" ><a href=" mailto:akshaycoder07@gmail.com">Get In Touch</a></button>
+                    </div>
                 </div>
-               </div>
             </div>
             {/* <Loader/> */}
 
-            <SocialMedia/>
+            <SocialMedia />
 
-            <Aboutme/>
+            <Aboutme />
 
-            <Projects/>
+            <Projects />
 
             <Footer />
         </section>
@@ -41,4 +41,3 @@ const HomePage = () => {
 }
 
 export default HomePage
-    
